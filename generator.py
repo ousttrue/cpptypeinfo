f7aaf83bed5fbcf0910919e31384ba8dd6abbdcd
ousttrue <ousttrue@gmail.com>
2019-09-15 21:40:10 +0900

    WIP generator

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 import pathlib
+import shutil
 from jinja2 import Template
 import cpptypeinfo
 HERE = pathlib.Path(__file__).absolute().parent
@@ -6,8 +7,11 @@ CIMGUI_H = HERE / 'libs/cimgui/cimgui.h'
 
 
 def generate_enum(enum: cpptypeinfo.Enum, root: pathlib.Path, namespace: str,
-                  is_flags: bool):
-    dst = root / f'{enum.type_name}.cs'
+                  is_flags: bool, typename_filter, valuename_filter):
+
+    type_name = typename_filter(enum.type_name)
+
+    dst = root / f'{type_name}.cs'
     t = Template('''// python generate
 namespace {{ namespace }}
 {
@@ -24,16 +28,81 @@ namespace {{ namespace }}
         f.write(
             t.render(namespace=namespace,
                      attribute='[Flags]' if is_flags else '',
-                     type_name=enum.type_name,
-                     values=enum.values))
+                     type_name=type_name,
+                     values=[
+                         valuename_filter(enum.type_name, v)
+                         for v in enum.values
+                     ]))
+
+
+def generate_typedef(typedef: cpptypeinfo.Typedef, root: pathlib.Path,
+                     namespace: str):
+
+    if isinstance(
+            typedef.src,
+            cpptypeinfo.Struct) and typedef.type_name == typedef.src.type_name:
+        # skip typedef struct same name
+        return
+
+    dst = root / f'{typedef.type_name}.cs'
+
+    t = Template('''// python generate
+namespace {{ namespace }}
+{
+    public struct {{ type_name }}
+    {
+        public {{ type }} Value;
+    }
+}
+''')
+
+    typedef_type = str(typedef.src)
+    if isinstance(typedef.src, cpptypeinfo.Pointer):
+        typedef_type = 'IntPtr'
+
+    with open(dst, 'w') as f:
+        f.write(
+            t.render(namespace=namespace,
+                     type_name=typedef.type_name,
+                     type=typedef_type))
 
 
 def generate(ns: cpptypeinfo.Namespace, root: pathlib.Path):
     root.mkdir(parents=True, exist_ok=True)
 
+    def typename_filter(src):
+        if src.endswith('_'):
+            return src[:-6]
+        else:
+            return src
+
+    def valuename_filter(type_name, src):
+        if src.name.startswith(type_name):
+            return cpptypeinfo.EnumValue(src.name[len(type_name):], src.value)
+        else:
+            return src
+
+    namespace_name = 'SharpImGui'
+    removes = []
     for k, v in ns.user_type_map.items():
         if isinstance(v, cpptypeinfo.Enum):
-            generate_enum(v, root, 'SharpImGui', v.type_name.endswith('Flags_'))
+
+            generate_enum(v, root, namespace_name,
+                          v.type_name.endswith('Flags_'), typename_filter,
+                          valuename_filter)
+            removes.append(v.type_name[:-1])
+
+    for k, v in ns.user_type_map.items():
+        if isinstance(v, cpptypeinfo.Enum):
+            pass
+        elif isinstance(v, cpptypeinfo.Typedef):
+            if v.type_name in removes:
+                continue
+            generate_typedef(v, root, namespace_name)
+        # elif isinstance(v, cpptypeinfo.Struct):
+        #     generate_struct(v, root)
+        else:
+            raise Exception()
 
 
 def main(dst: pathlib.Path):
@@ -41,6 +110,8 @@ def main(dst: pathlib.Path):
     cpptypeinfo.parse_header(CIMGUI_H,
                              cpp_flags=['-DCIMGUI_DEFINE_ENUMS_AND_STRUCTS'])
     cpptypeinfo.pop_namespace()
+
+    shutil.rmtree(dst)
 
     for level, ns in root.traverse():
         if not isinstance(ns, cpptypeinfo.Struct):
