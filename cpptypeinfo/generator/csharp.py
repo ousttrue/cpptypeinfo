8d588a81f7772ee75385007f4c43cd352e82844c
ousttrue <ousttrue@gmail.com>
2019-09-17 00:42:37 +0900

    CSMarshalType

--- cpptypeinfo/generator/csharp.py
+++ cpptypeinfo/generator/csharp.py
@@ -1,5 +1,5 @@
 import pathlib
-from typing import Optional, Tuple, Dict, NamedTuple
+from typing import Dict, NamedTuple
 import cpptypeinfo
 from jinja2 import Template
 
@@ -7,52 +7,61 @@ HEADLINE = f'// generated cpptypeinfo-{cpptypeinfo.VERSION}'
 USING = '''using System;
 using System.Runtime.InteropServices;'''
 
-cs_type_map: Dict[cpptypeinfo.PrimitiveType, Tuple[Optional[str], str]] = {
-    cpptypeinfo.Int8(): (None, 'sbyte'),
-    cpptypeinfo.Int16(): (None, 'short'),
-    cpptypeinfo.Int32(): (None, 'int'),
-    cpptypeinfo.Int64(): (None, 'long'),
-    cpptypeinfo.UInt8(): (None, 'byte'),
-    cpptypeinfo.UInt16(): (None, 'ushort'),
-    cpptypeinfo.UInt32(): (None, 'uint'),
-    cpptypeinfo.UInt64(): (None, 'ulong'),
-    cpptypeinfo.Float(): (None, 'float'),
-    cpptypeinfo.Double(): (None, 'double'),
-    cpptypeinfo.Bool(): ('MarshalAs(UnmanagedType.U1)', 'bool'),
+
+class CSMarshalType(NamedTuple):
+    type: str
+    marshal_as: str = ''
+
+
+cstype_map: Dict[cpptypeinfo.PrimitiveType, CSMarshalType] = {
+    cpptypeinfo.Int8(): CSMarshalType('sbyte'),
+    cpptypeinfo.Int16(): CSMarshalType('short'),
+    cpptypeinfo.Int32(): CSMarshalType('int'),
+    cpptypeinfo.Int64(): CSMarshalType('long'),
+    cpptypeinfo.UInt8(): CSMarshalType('byte'),
+    cpptypeinfo.UInt16(): CSMarshalType('ushort'),
+    cpptypeinfo.UInt32(): CSMarshalType('uint'),
+    cpptypeinfo.UInt64(): CSMarshalType('ulong'),
+    cpptypeinfo.Float(): CSMarshalType('float'),
+    cpptypeinfo.Double(): CSMarshalType('double'),
+    cpptypeinfo.Bool(): CSMarshalType('bool', 'MarshalAs(UnmanagedType.U1)'),
 }
 
 
-def to_cs(decl: cpptypeinfo.Type) -> Tuple[Optional[str], str]:
+def to_cs(decl: cpptypeinfo.Type) -> CSMarshalType:
+    '''
+    ToDo: context: struct field, function return, function param
+    '''
     if not decl:
         # bug
         print('### bug ###')
-        return (None, 'IntPtr')
+        return CSMarshalType('IntPtr')
 
     if isinstance(decl, cpptypeinfo.PrimitiveType):
-        cs_type = cs_type_map.get(decl)
+        cs_type = cstype_map.get(decl)
         if cs_type:
             return cs_type
 
     if isinstance(decl, cpptypeinfo.Void):
-        return (None, 'void')
+        return CSMarshalType('void')
     elif isinstance(decl, cpptypeinfo.Array):
-        return (None, 'IntPtr')
+        return CSMarshalType('IntPtr')
     elif isinstance(decl, cpptypeinfo.Pointer):
         if isinstance(decl.typeref.ref, cpptypeinfo.Bool):
-            return ('MarshalAs(UnmanagedType.U1)', 'ref bool')
+            return CSMarshalType('ref bool', 'MarshalAs(UnmanagedType.U1)')
         else:
-            return (None, 'IntPtr')
+            return CSMarshalType('IntPtr')
     elif isinstance(decl, cpptypeinfo.Function):
-        return (None, 'IntPtr')
+        return CSMarshalType('IntPtr')
     elif isinstance(decl, cpptypeinfo.Enum):
-        return (None, decl.type_name)
+        return CSMarshalType(decl.type_name)
     elif isinstance(decl, cpptypeinfo.Struct):
-        return (None, decl.type_name)
+        return CSMarshalType(decl.type_name)
     elif isinstance(decl, cpptypeinfo.Typedef):
         if isinstance(decl.get_concrete_type(), cpptypeinfo.Function):
-            return (None, 'IntPtr')
+            return CSMarshalType('IntPtr')
         else:
-            return (None, decl.type_name)
+            return CSMarshalType(decl.type_name)
     else:
         raise NotImplementedError(str(decl))
 
@@ -125,13 +134,13 @@ namespace {{ namespace }}
 }
 ''')
 
-    typedef_attr, typedef_type = to_cs(typedef.typeref.ref)
-    if typedef_attr:
-        typedef_attr = f'[typedef_attr]'
+    cstype = to_cs(typedef.typeref.ref)
+    if cstype.marshal_as:
+        typedef_attr = f'[{cstype.marshal_as}]'
     else:
         typedef_attr = ''
 
-    typedef_type = f'{typedef_attr}public {typedef_type} Value'
+    typedef_type = f'{typedef_attr}public {cstype.type} Value'
 
     with open(context.path, 'w') as f:
         f.write(
@@ -164,13 +173,13 @@ namespace {{ namespace }}
 ''')
 
     def field_str(f: cpptypeinfo.Field):
-        field_attr, field_type = to_cs(f.typeref.ref)
-        if field_attr:
-            field_attr = f'[{field_attr}]'
+        cstype = to_cs(f.typeref.ref)
+        if cstype.marshal_as:
+            field_attr = f'[{cstype.marshal_as}]'
         else:
             field_attr = ''
         return f'''// offsetof: {f.offset}
-        {field_attr}public {field_type} {f.name}'''
+        {field_attr}public {cstype.type} {f.name}'''
 
     with open(context.path, 'w') as f:
         f.write(
@@ -185,23 +194,23 @@ namespace {{ namespace }}
 
 def generate_functions(root_ns: cpptypeinfo.Namespace, context: CSContext):
     def to_cs_param(p: cpptypeinfo.Param):
-        param_attr, param = to_cs(p.typeref.ref)
-        if param_attr:
-            param_attr = f'[{param_attr}]'
+        cstype = to_cs(p.typeref.ref)
+        if cstype.marshal_as:
+            param_attr = f'[{cstype.marshal_as}]'
         else:
             param_attr = ''
-        return f'{param_attr}{param} {escape_symbol(p.name)}'
+        return f'{param_attr}{cstype.type} {escape_symbol(p.name)}'
 
     def function_str(v: cpptypeinfo.Function):
         params = [to_cs_param(p) for p in v.params]
-        ret_attr, ret = to_cs(v.result.ref)
-        if ret_attr:
-            ret_attr = f'\n        [return: {ret_attr}]\n'
+        cstype = to_cs(v.result.ref)
+        if cstype.marshal_as:
+            ret_attr = f'\n        [return: {cstype.marshal_as}]\n'
         else:
             ret_attr = ''
         return f'''// {v.file.name}:{v.line}
         [DllImport(DLLNAME)]{ret_attr}
-        public static extern {ret} {v.name}({", ".join(params)});'''
+        public static extern {cstype.type} {v.name}({", ".join(params)});'''
 
     values = []
     for ns in root_ns.traverse():
