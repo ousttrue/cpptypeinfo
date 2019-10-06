553e31906b38f430fa5edb8d58136864e192b0ac
ousttrue <ousttrue@gmail.com>
2019-10-06 15:08:58 +0900

    test

--- tests/test_cindex_function.py
+++ tests/test_cindex_function.py
@@ -19,6 +19,23 @@ class CIndexFunctionTests(unittest.TestCase):
         self.assertEqual(1, len(func.params))
         self.assertEqual(TypeRef(cpptypeinfo.Int32()), func.params[0].typeref)
 
+    def test_excpt(self) -> None:
+        parser = cpptypeinfo.TypeParser()
+        cpptypeinfo.parse_source(parser,
+                                 '''
+        int __cdecl __C_specific_handler(
+            struct _EXCEPTION_RECORD*   ExceptionRecord,
+            void*                       EstablisherFrame,
+            struct _CONTEXT*            ContextRecord,
+            struct _DISPATCHER_CONTEXT* DispatcherContext
+            );
+''',
+                                 debug=True)
+        func = parser.root_namespace.functions[0]
+        self.assertEqual(TypeRef(cpptypeinfo.Void(), False), func.result)
+        self.assertEqual(1, len(func.params))
+        self.assertEqual(TypeRef(cpptypeinfo.Int32()), func.params[0].typeref)
+
 
 if __name__ == '__main__':
     unittest.main()
