38b219ec5683dbba380e6f5ca2074c1ea0ccfaa9
ousttrue <ousttrue@gmail.com>
2019-10-16 21:36:06 +0900

    remove nothrow

--- cpptypeinfo/languages/dlang.py
+++ cpptypeinfo/languages/dlang.py
@@ -244,7 +244,7 @@ def dlang_function(d: TextIO, m: Function, indent='') -> None:
     ret = m.result if m.result else 'void'
     params = ', '.join(f'{to_d(p.typeref)} {p.name}' for p in m.params)
     extern_c = 'extern(C) ' if m.extern_c else ''
-    d.write(f'{indent}{extern_c}{to_d(ret)} {m.name}({params}) nothrow;\n')
+    d.write(f'{indent}{extern_c}{to_d(ret)} {m.name}({params});\n')
 
 
 def escape_symbol(name: str) -> str:
